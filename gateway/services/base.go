5efc5cac8095fbb7d278af068c2adff629c1ad81
zhizhong.xu <zhizhong.xu@shopee.com>
2021-03-18 21:03:55 +0800

    (gateway): 确定定位API基本的输入输出约定

---
+++ gateway/services/base.go
@@ -0,0 +1,10 @@
+package services
+
+type baseResp struct {
+	Code int `json:"code"`
+	Msg string `json:"msg,omitempty"`
+}
+
+func Resp(code int, msg string) baseResp {
+	return baseResp{code, msg}
+}
--- gateway/services/locate.go
+++ gateway/services/locate.go
@@ -19,10 +19,28 @@ func NewLocateReq() *LocateReq {
 	return &LocateReq{}
 }
 
+type location struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+type LocateResp struct {
+	baseResp
+	Data location `json:"data,omitempty"`
+}
+
+func NewLocateResp(x, y int) *LocateResp {
+	return &LocateResp{
+		Resp(0, ""),
+		location{x, y},
+	}
+}
 func Locate(c *gin.Context) {
 	
 	req := NewLocateReq()
 	c.BindJSON(req)
 
-	c.JSON(http.StatusOK, req)
+	//TODO rpc
+
+	location := NewLocateResp(0, 0)
+	c.JSON(http.StatusOK, location)
 }
