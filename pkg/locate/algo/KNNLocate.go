1fb9dcd4570e89a7991a5cd8ed30773be2bed151
zhizhong.xu <zhizhong.xu@shopee.com>
2021-05-04 23:58:24 +0800

    完善定位数据类定义，完成KNN定位算法实现

--- pkg/locate/algo/KNNLocate.go
+++ pkg/locate/algo/KNNLocate.go
@@ -7,23 +7,18 @@ import (
 	"github.com/1000Delta/wifi-locate/pkg/locate"
 )
 
-// APVector define a general mode to get vector value
-type APVector interface {
-	GetVecVal(dimension int) int64
-	GetLocation() (float64, float64)
-}
-
 type KNNLocator struct {
 	k int // k nums target to locate
 }
 
-func NewKNNLocator(k int) *KNNLocator {
+func NewKNNLocator(k int) locate.Locator {
 	return &KNNLocator{k: k}
 }
 
-func (cfg KNNLocator) Locate(scanVec APVector, vecListDB []APVector) locate.LocationInfo {
+// Locate implement locate algo for KNN
+func (cfg KNNLocator) Locate(scanVec locate.APVector, vecListDB []locate.APVector) locate.LocationInfo {
 	diffList := make(apDiffList, len(vecListDB))
-	
+
 	// 计算欧式距离
 	for i, baseVec := range vecListDB {
 		diffList[i] = getDiff(baseVec, scanVec)
@@ -39,10 +34,10 @@ func (cfg KNNLocator) Locate(scanVec APVector, vecListDB []APVector) locate.Loca
 type apDiff struct {
 	// Euclidean Distance
 	EuDist  float64
-	BaseVec APVector
+	BaseVec locate.APVector
 }
 
-func getDiff(baseVec, dbVec APVector) *apDiff {
+func getDiff(baseVec, dbVec locate.APVector) *apDiff {
 	euDist := math.Sqrt(
 		math.Pow(float64(baseVec.GetVecVal(0)-dbVec.GetVecVal(0)), 2) +
 			math.Pow(float64(baseVec.GetVecVal(1)-dbVec.GetVecVal(1)), 2) +
@@ -73,5 +68,5 @@ func getAvgLocation(diffList apDiffList) (float64, float64) {
 
 	// TODO 确认结果正确性，论文中多计算了一个欧氏距离
 
-	return xall/countF , yall/countF
+	return xall / countF, yall / countF
 }
--- pkg/locate/interfaces.go
+++ pkg/locate/interfaces.go
@@ -1,6 +1,13 @@
 package locate
 
+// APVector define a general mode to get vector value
+type APVector interface {
+	GetVecVal(dimension int) int64
+	GetLocation() (float64, float64)
+}
+
+// Locator is common interface of all locate algorithms
 type Locator interface {
 	// Locate device by compute current apList1 with apList2 in database.
-	Locate([]*APInfo, []*APInfo) LocationInfo
+	Locate(APVector, []APVector) LocationInfo
 }
