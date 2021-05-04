af6c8efd774f9d9f9a75df95672a16fe0bc3c9a7
zhizhong.xu <zhizhong.xu@shopee.com>
2021-05-04 18:08:52 +0800

    完善 locate 类型定义

--- pkg/locate/types.go
+++ pkg/locate/types.go
@@ -3,9 +3,10 @@ package locate
 // APInfo is useful info of WIFI AP for location
 type APInfo struct {
 	BSSID string `json:"bssid"`
-	RSSI  string `json:"rssi"` // Recieved Signal Strength Indicator
+	RSSI  int64  `json:"rssi"` // Recieved Signal Strength Indicator
 }
+
 type LocationInfo struct {
-	X int `json:"x"`
-	Y int `json:"y"`
+	X float64 `json:"x"`
+	Y float64 `json:"y"`
 }
