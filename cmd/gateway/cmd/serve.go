808480a54f2b7071573caa2fd246e98e225475cc
zhizhong.xu <zhizhong.xu@shopee.com>
2021-05-11 00:38:23 +0800

    add: gateway 添加 cobra + viper 构建启动命令

---
+++ cmd/gateway/cmd/serve.go
@@ -0,0 +1,57 @@
+/*
+Copyright © 2021 DeltaX 13975001197@163.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/1000Delta/wifi-locate/pkg/gateway"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "run gateway service",
+	Long: `
+Run gateway service, support wifi-locate request.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// init server
+		e := gin.Default()
+
+		gateway.RegisterRouter(e)
+
+		port := viper.GetInt("port")
+
+		e.Run(fmt.Sprintf(":%d", port))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serveCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serveCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,15 +1,22 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import (
-	"github.com/1000Delta/wifi-locate/pkg/gateway"
-	"github.com/gin-gonic/gin"
-)
+import "github.com/1000Delta/wifi-locate/cmd/gateway/cmd"
 
 func main() {
-	// init
-	e := gin.Default()
-
-	gateway.RegisterRouter(e)
-
-	e.Run(":8080")
+	cmd.Execute()
 }
