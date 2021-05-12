a3c7d2601ef30609091c60968bae32393a7928b2
zhizhong.xu <zhizhong.xu@shopee.com>
2021-05-13 01:39:20 +0800

    添加尝试调用函数，便于调用资源

---
+++ pkg/common/utils/caller.go
@@ -0,0 +1,56 @@
+package utils
+
+import (
+	"errors"
+	"time"
+)
+
+var ErrTimeout = errors.New("call failed until timeout")
+
+// CallUntilNoError will do callback and catch error,
+// then sleep current goroutine by delay(will be up from initDelay to maxDelay),
+// until the callback return nil.
+//
+// initDelay will by up by 2 power until it great than or equal than maxDelay.
+func CallUntilNoError(callback func(currentDelay time.Duration) error, initDelay time.Duration, maxDelay time.Duration) {
+	delay := initDelay
+	for {
+		err := callback(delay)
+		if err == nil {
+			return
+		}
+		// 延时当前线程
+		time.Sleep(time.Second * delay)
+		if delay < maxDelay {
+			delay *= 2
+		} else if delay > maxDelay {
+			delay = maxDelay
+		}
+	}
+}
+
+// CallUntilNoErrorWithTimeout means call CallUntilNoError
+// but add a timeout that doing will not be endless
+func CallUntilNoErrorWithTimeout(
+	callback func(currentDelay time.Duration) error,
+	initDelay time.Duration, maxDelay time.Duration, timeout time.Duration) error {
+	outTicker := time.NewTicker(timeout)
+	doneChan := make(chan struct{})
+
+	go CallUntilNoError(func(currentDelay time.Duration) error {
+		err := callback(currentDelay)
+		if err != nil {
+			return err
+		}
+		// 标记执行完成
+		doneChan <- struct{}{}
+		return nil
+	}, initDelay, maxDelay)
+
+	select {
+	case <-outTicker.C:
+		return ErrTimeout
+	case <-doneChan:
+		return nil
+	}
+}
