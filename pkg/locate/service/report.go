d564b060e93bc5105907b9bdc68d8d350895215a
zhizhong.xu <zhizhong.xu@shopee.com>
2021-05-14 01:27:31 +0800

    修改 地图创建 返回 ID

--- pkg/locate/service/report.go
+++ pkg/locate/service/report.go
@@ -25,26 +25,30 @@ func (LocateService) Collect(req CollectReq, resp *CollectResp) error {
 }
 
 type CreateMapReq struct {
-	Name string
-	Path string
-	Width int64
+	Name   string
+	Path   string
+	Width  int64
 	Height int64
 }
 
-type CreateMapResp struct {}
+type CreateMapResp struct {
+	mapID uint
+}
 
 // CreateMap
 func (LocateService) CreateMap(req CreateMapReq, resp *CreateMapResp) error {
 	m := &model.LocationMap{
-		Name: req.Name,
-		Path: req.Path,
-		Width: req.Width,
+		Name:   req.Name,
+		Path:   req.Path,
+		Width:  req.Width,
 		Height: req.Height,
 	}
 	if err := m.Add(); err != nil {
 		log.Printf("add map error, msg = %v", err)
 		return err
 	}
+
+	*resp = CreateMapResp{mapID: m.ID}
 
 	return nil
 }
